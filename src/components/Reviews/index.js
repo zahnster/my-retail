d7952644b17d0da923b25ebf4b9afd1c4bd35fb4
Jade Pennig <jade@pennig.name>
2017-03-14 21:45:52 -0500

    Reviews UI styling and a few other UI adjustments. Major UI layout aspects are now complete.

--- src/components/Reviews/index.js
+++ src/components/Reviews/index.js
@@ -2,17 +2,35 @@ import React, { Component } from 'react'
 
 // Component imports
 import TopReview from '../TopReview'
+import RatingStars from '../RatingStars'
+
+// Component stylesheet
+import './style.css'
 
 class Reviews extends Component {
 	render() {
 		const reviews = this.props.reviews[0]
 		const proReview = reviews.Pro[0]
 		const conReview = reviews.Con[0]
+		const reviewCount = reviews.Reviews.length
 
 		return (
 			<div className='reviews'>
-				<TopReview type='Pro' starRange='4-5' review={proReview} />
-				<TopReview type='Con' starRange='1-2' review={conReview} />
+				<div className='review-summary'>
+					<div>
+						<RatingStars rating={5} />
+						overall						
+					</div>
+
+					<div className='view-all-reviews'>
+						<a href='#'>View all {reviewCount} reviews</a>
+					</div>
+				</div>
+
+				<div className='top-reviews'>
+					<TopReview type='Pro' starRange='4-5' review={proReview} />
+					<TopReview type='Con' starRange='1-2' review={conReview} />
+				</div>
 			</div>
 		)
 	}
