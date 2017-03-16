16d8270d83648979011b05316ab058ed0069c3a3
Jade Pennig <jade@pennig.name>
2017-03-16 13:30:25 -0500

    Price component testing and refactor

--- src/components/Price/index.js
+++ src/components/Price/index.js
@@ -1,28 +1,26 @@
-import React, { Component } from 'react'
+import React from 'react'
 
 // Component stylesheet
 import './style.css'
 
-class Price extends Component {
-	render() {
-		const priceData = this.props.offers[0].OfferPrice[0]
+function Price(props) {
+	const priceData = props.offers[0].OfferPrice[0]
 
-		return (
-			<div className='price'>
-				<p>
-					{priceData.formattedPriceValue}
-					
-					<span className='price-type'>
-						{priceData.priceQualifier}
-					</span>
-				</p>
-			</div>
-		)
-	}
+	return (
+		<div className='price'>
+			<p>
+				{priceData.formattedPriceValue}
+				
+				<span className='price-type'>
+					{priceData.priceQualifier}
+				</span>
+			</p>
+		</div>
+	)
 }
 
 Price.propTypes = {
-	offers: React.PropTypes.array
+	offers: React.PropTypes.array.isRequired
 }
 
 export default Price
