785cc934ab71f09b9ea0b23427e8ee17e401e013
Jade Pennig <jade@pennig.name>
2017-03-16 21:35:35 -0500

    Carousel AX work.

--- src/components/Picture/index.js
+++ src/components/Picture/index.js
@@ -1,15 +1,18 @@
 import React from 'react'
 
 function Picture(props) {
+	const altText = props.alt || ''
+
 	return (
 		<div className={`picture ${props.cssClass}`}>
-			<img src={props.image} alt='' />
+			<img src={props.image} alt={altText} />
 		</div>
 	)
 }
 
 Picture.propTypes = {
 	image: React.PropTypes.string.isRequired,
+	alt: React.PropTypes.string,
 	cssClass: React.PropTypes.string
 }
 
